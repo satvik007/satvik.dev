027ad59faf25ae2a79230969444af5b5051b3da1
Satvik <satvikchoudhary@gmail.com>
2022-05-06 01:03:53 +0530

    fix(jest): added fetcher and fixed jest related issues

---
+++ lib/__tests__/fetcher.test.ts
@@ -0,0 +1,9 @@
+import { fetchBooks } from '@/lib/fetcher'
+
+describe('Test suite for fetcher', () => {
+  it('run fetchBooks', async () => {
+    const books = await fetchBooks()
+    expect(books).toBeArray()
+    console.log(books)
+  })
+})
